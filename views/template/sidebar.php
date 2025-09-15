deadbace7a3ef6260b45de505f23adc16ae6ebca
Hanif Luqmanul Hakim <hanifluqmanulhakim20@gmail.com>
2025-09-15 17:53:56 +0700

    Create sidebar.php

---
+++ views/template/sidebar.php
@@ -0,0 +1,84 @@
+<div id="layoutSidenav">
+    <div id="layoutSidenav_nav">
+        <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+            <div class="sb-sidenav-menu">
+                <div class="nav">
+                    <div class="sb-sidenav-menu-heading">Core</div>
+                    <a class="nav-link" href="<?php echo site_url('dashboard'); ?>">
+                        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                        Dashboard
+                    </a>
+                    <div class="sb-sidenav-menu-heading">Interface</div>
+                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                        <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                        Layouts
+                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                    </a>
+                    <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                        <nav class="sb-sidenav-menu-nested nav">
+                            <a class="nav-link" href="<?php echo base_url('assets/layout-static.html'); ?>">Static Navigation</a>
+                            <a class="nav-link" href="<?php echo base_url('assets/layout-sidenav-light.html'); ?>">Light Sidenav</a>
+                        </nav>
+                    </div>
+                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                        <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                        Pages
+                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                    </a>
+                    <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                        <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
+                                Authentication
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="<?php echo base_url('assets/login.html'); ?>">Login</a>
+                                    <a class="nav-link" href="<?php echo base_url('assets/register.html'); ?>">Register</a>
+                                    <a class="nav-link" href="<?php echo base_url('assets/password.html'); ?>">Forgot Password</a>
+                                </nav>
+                            </div>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
+                                Error
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="<?php echo base_url('assets/401.html'); ?>">401 Page</a>
+                                    <a class="nav-link" href="<?php echo base_url('assets/404.html'); ?>">404 Page</a>
+                                    <a class="nav-link" href="<?php echo base_url('assets/500.html'); ?>">500 Page</a>
+                                </nav>
+                            </div>
+                        </nav>
+                    </div>
+                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseAritmatika" aria-expanded="false" aria-controls="collapseAritmatika">
+                        <div class="sb-nav-link-icon"><i class="fas fa-calculator"></i></div>
+                        Aritmatika
+                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                    </a>
+                    <div class="collapse" id="collapseAritmatika" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                        <nav class="sb-sidenav-menu-nested nav">
+                            <a class="nav-link" href="<?php echo site_url('hitung'); ?>">Menu Aritmatika</a>
+                            <a class="nav-link" href="<?php echo site_url('hitung/penjumlahan'); ?>">Penjumlahan</a>
+                            <a class="nav-link" href="<?php echo site_url('hitung/pengurangan'); ?>">Pengurangan</a>
+                            <a class="nav-link" href="<?php echo site_url('hitung/perkalian'); ?>">Perkalian</a>
+                            <a class="nav-link" href="<?php echo site_url('hitung/pembagian'); ?>">Pembagian</a>
+                        </nav>
+                    </div>
+                    <div class="sb-sidenav-menu-heading">Addons</div>
+                    <a class="nav-link" href="<?php echo base_url('assets/charts.html'); ?>">
+                        <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                        Charts
+                    </a>
+                    <a class="nav-link" href="<?php echo base_url('assets/tables.html'); ?>">
+                        <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                        Tables
+                    </a>
+                </div>
+            </div>
+            <div class="sb-sidenav-footer">
+                <div class="small">Logged in as:</div>
+                Toko Kuker TI
+            </div>
+        </nav>
+    </div>
